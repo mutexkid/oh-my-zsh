925f32f0c5756104a27d3d151afb20cfa39b09cf
Josh Skeen <josh@joshskeen.com>
2013-09-23 14:54:48 -0400

    adds ghist

---
+++ ghist.zsh
@@ -0,0 +1,32 @@
+" Press ? for help
+
+.. (up a dir)
+/Users/joshskeen/.oh-my-zsh/
+▸ .git/
+▸ custom/
+▾ lib/
+    aliases.zsh
+    completion.zsh
+    correction.zsh
+    directories.zsh
+    edit-command-line.zsh
+    functions.zsh
+    git.zsh
+    grep.zsh
+    history.zsh
+    key-bindings.zsh
+    misc.zsh
+    rbenv.zsh
+    rvm.zsh
+    spectrum.zsh
+    termsupport.zsh
+    theme-and-appearance.zsh
+▸ log/
+▸ plugins/
+▸ templates/
+▸ themes/
+▸ tools/
+  .gitignore
+  MIT-LICENSE.txt
+  oh-my-zsh.sh
+  README.textile
